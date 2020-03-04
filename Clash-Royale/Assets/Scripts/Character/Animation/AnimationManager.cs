d702fbd9e2a32da9853a0b3e6deaa13df17e3876
YesilyurtGurkan <gurkan@ganover.com>
2020-03-04 18:00:50 +0300

    Animation rotation fix.Angle add

---
+++ Clash-Royale/Assets/Scripts/Character/Animation/AnimationManager.cs
@@ -0,0 +1,60 @@
+﻿
+using Pathfinding;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    public Seeker seeker;
+    public Transform target;
+
+    int currentWayPoint;
+    Pathfinding.Path path;
+
+    Animator anim;
+    private void Awake()
+    {
+        seeker = GetComponent<Seeker>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        seeker.StartPath(transform.position, target.position, onPathCompleted);
+
+    }
+    private void Update()
+    {
+    
+    }
+
+    private void FixedUpdate()
+    {
+        if (path == null)
+        {
+            return;
+        }
+        if (currentWayPoint >= path.vectorPath.Count)
+        {
+            return;
+        }
+        Vector3 dir = ( transform.position- path.vectorPath[currentWayPoint] ).normalized;
+        //Report the path using the delegate
+        anim.SetFloat("InputX",dir.x);
+
+
+        anim.SetFloat("InputY",dir.y);
+    }
+    void onPathCompleted(Pathfinding.Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+        else
+        {
+            Debug.Log(p.error);
+        }
+    }
+
+}
--- Clash-Royale/Assets/Scripts/Character/CharacterController.cs
+++ Clash-Royale/Assets/Scripts/Character/CharacterController.cs
@@ -5,17 +5,84 @@ public class CharacterController : MonoBehaviour {
 
     [Header("Debug")]
     [SerializeField]
+    float _slope;
+    float _angleCharacter;
     private AIPath _AIPath = null;
-
+    Animator anim;
     private void Awake() {
         _AIPath = GetComponent<AIPath>();
+        anim = GetComponentInChildren<Animator>();
+
+
+
+
     }
 
     private void Update() {
         if (_AIPath == null)
             return;
 
-        Debug.Log(_AIPath.desiredVelocity);
+        anim.SetFloat("InputX", _AIPath.desiredVelocity.x/ _AIPath.maxSpeed);
+        anim.SetFloat("InputY", _AIPath.desiredVelocity.y/ _AIPath.maxSpeed);
+       // Debug.Log(_AIPath.desiredVelocity / _AIPath.maxSpeed);
+
+        if (_AIPath.desiredVelocity.y<0)
+        {
+            _angleCharacter = 360 - Vector2.Angle(_AIPath.desiredVelocity / _AIPath.maxSpeed, new Vector2(1, 0));
+            //Debug.Log(360-Vector2.Angle(_AIPath.desiredVelocity / _AIPath.maxSpeed, new Vector2(1, 0)));
+        }
+        else
+        {
+            _angleCharacter = Vector2.Angle(_AIPath.desiredVelocity / _AIPath.maxSpeed, new Vector2(1, 0));
+          //  Debug.Log(Vector2.Angle(_AIPath.desiredVelocity / _AIPath.maxSpeed, new Vector2(1, 0)));
+
+        }
+
+
+
+        if (_angleCharacter<=30&& _angleCharacter >330)
+        {
+            anim.SetFloat("InputX",1);
+            anim.SetFloat("InputY",0);
+        }
+        else if (_angleCharacter>30&& _angleCharacter <= 60)
+        {
+            anim.SetFloat("InputX", .5f);
+            anim.SetFloat("InputY", .5f);
+
+        }
+        else if (_angleCharacter > 60&& _angleCharacter <= 120)
+        {
+            anim.SetFloat("InputX", 0);
+            anim.SetFloat("InputY", 1f);
+
+        }
+        else if (_angleCharacter > 120 && _angleCharacter <= 150)
+        {
+            anim.SetFloat("InputX", -.5f);
+            anim.SetFloat("InputY", .5f);
+        }
+        else if (_angleCharacter > 150 && _angleCharacter <= 210)
+        {
+            anim.SetFloat("InputX", -1);
+            anim.SetFloat("InputY", 0);
+        }
+        else if (_angleCharacter > 210 && _angleCharacter <= 240)
+        {
+            anim.SetFloat("InputX", -.5f);
+            anim.SetFloat("InputY", -.5f);
+        }
+        else if (_angleCharacter > 240 && _angleCharacter <= 300)
+        {
+            anim.SetFloat("InputX", 0);
+            anim.SetFloat("InputY", -1);
+        }
+        else if (_angleCharacter > 300 && _angleCharacter <= 330)
+        {
+            anim.SetFloat("InputX", .5f);
+            anim.SetFloat("InputY", -.5f);
+        }
+
     }
 
 
