649576b9a75a7dff6aa75bfeb95752b8683c971c
gkaragoz <annotto3@gmail.com>
2020-03-04 16:27:40 +0300

    Create CharacterController.cs script basics.

---
+++ Clash-Royale/Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using Pathfinding;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    [Header("Debug")]
+    [SerializeField]
+    private AIPath _AIPath = null;
+
+    private void Awake() {
+        _AIPath = GetComponent<AIPath>();
+    }
+
+    private void Update() {
+        if (_AIPath == null)
+            return;
+
+        Debug.Log(_AIPath.desiredVelocity);
+    }
+
+
+}
