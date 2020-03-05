3b7f7c97aac6076d64c13643ce6ac4e84dafbbf7
YesilyurtGurkan <gurkan@ganover.com>
2020-03-05 10:01:34 +0300

    Refactor character rotation.

--- Clash-Royale/Assets/Scripts/Character/CharacterController.cs
+++ Clash-Royale/Assets/Scripts/Character/CharacterController.cs
@@ -1,57 +1,56 @@
 ﻿using Pathfinding;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour {
+public class CharacterController : MonoBehaviour
+{
 
-    [Header("Debug")]
-    [SerializeField]
-    float _slope;
+
     float _angleCharacter;
     private AIPath _AIPath = null;
+    private Vector2 _unitVectorRight;
     Animator anim;
-    private void Awake() {
+
+
+    private void Awake()
+    {
         _AIPath = GetComponent<AIPath>();
         anim = GetComponentInChildren<Animator>();
-
-
-
-
+        _unitVectorRight = new Vector2(1, 0); // needed for getting standart angle of movement vector
     }
 
-    private void Update() {
+    private void Update()
+    {
         if (_AIPath == null)
             return;
 
-        anim.SetFloat("InputX", _AIPath.desiredVelocity.x/ _AIPath.maxSpeed);
-        anim.SetFloat("InputY", _AIPath.desiredVelocity.y/ _AIPath.maxSpeed);
-       // Debug.Log(_AIPath.desiredVelocity / _AIPath.maxSpeed);
 
-        if (_AIPath.desiredVelocity.y<0)
+        // Vector2.Angle gives a float value between 0-180. Needed negative y area.
+        if (_AIPath.desiredVelocity.y < 0)
         {
-            _angleCharacter = 360 - Vector2.Angle(_AIPath.desiredVelocity / _AIPath.maxSpeed, new Vector2(1, 0));
-            //Debug.Log(360-Vector2.Angle(_AIPath.desiredVelocity / _AIPath.maxSpeed, new Vector2(1, 0)));
+            _angleCharacter = 360 - Vector2.Angle(_AIPath.desiredVelocity / _AIPath.maxSpeed, _unitVectorRight);
         }
         else
         {
-            _angleCharacter = Vector2.Angle(_AIPath.desiredVelocity / _AIPath.maxSpeed, new Vector2(1, 0));
-          //  Debug.Log(Vector2.Angle(_AIPath.desiredVelocity / _AIPath.maxSpeed, new Vector2(1, 0)));
+            _angleCharacter = Vector2.Angle(_AIPath.desiredVelocity / _AIPath.maxSpeed, _unitVectorRight);
 
         }
 
 
 
-        if (_angleCharacter<=30&& _angleCharacter >330)
+        //Topdown rotationg angles
+
+        if (_angleCharacter <= 30 && _angleCharacter > 330)
         {
-            anim.SetFloat("InputX",1);
-            anim.SetFloat("InputY",0);
+            anim.SetFloat("InputX", 1);
+            anim.SetFloat("InputY", 0);
         }
-        else if (_angleCharacter>30&& _angleCharacter <= 60)
+        else if (_angleCharacter > 30 && _angleCharacter <= 60)
         {
             anim.SetFloat("InputX", .5f);
             anim.SetFloat("InputY", .5f);
 
         }
-        else if (_angleCharacter > 60&& _angleCharacter <= 120)
+        else if (_angleCharacter > 60 && _angleCharacter <= 120)
         {
             anim.SetFloat("InputX", 0);
             anim.SetFloat("InputY", 1f);
