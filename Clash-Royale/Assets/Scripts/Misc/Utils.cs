95b746705c539754568fd37c808abc873f4fca98
gkaragoz <annotto3@gmail.com>
2020-03-06 23:42:46 +0300

    Add IsPointerOverUIObject method inside Utils.cs script.

--- Clash-Royale/Assets/Scripts/Misc/Utils.cs
+++ Clash-Royale/Assets/Scripts/Misc/Utils.cs
@@ -19,4 +19,12 @@ public class Utils : MonoBehaviour {
     }
 #endif
 
+    public static bool IsPointerOverUIObject() {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
+
 }
