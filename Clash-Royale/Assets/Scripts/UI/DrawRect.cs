6e57856d2324582337c301def936c6fb80c485ba
gkaragoz <annotto3@gmail.com>
2020-03-06 21:56:22 +0300

    Create DrawRect.cs script to draw territories via OnGUI methods.

---
+++ Clash-Royale/Assets/Scripts/UI/DrawRect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public static class DrawRect {
+
+    private static readonly Texture2D backgroundTexture = Texture2D.whiteTexture;
+    private static readonly GUIStyle textureStyle = new GUIStyle { normal = new GUIStyleState { background = backgroundTexture } };
+
+    public static void DrawRectangle(Rect position, Color color, GUIContent content = null) {
+        var backgroundColor = GUI.backgroundColor;
+        GUI.backgroundColor = color;
+        GUI.Box(position, content ?? GUIContent.none, textureStyle);
+        GUI.backgroundColor = backgroundColor;
+    }
+
+    public static void LayoutBox(Color color, GUIContent content = null) {
+        var backgroundColor = GUI.backgroundColor;
+        GUI.backgroundColor = color;
+        GUILayout.Box(content ?? GUIContent.none, textureStyle);
+        GUI.backgroundColor = backgroundColor;
+    }
+}
