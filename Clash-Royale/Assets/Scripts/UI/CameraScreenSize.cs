a296ce28bce79a1c130c5ec0c7e0f14ac4be5280
gkaragoz <annotto3@gmail.com>
2020-03-09 12:48:15 +0300

    Finalize CameraScreenSize ortographic value dynamic by every devices.

--- Clash-Royale/Assets/Scripts/UI/CameraScreenSize.cs
+++ Clash-Royale/Assets/Scripts/UI/CameraScreenSize.cs
@@ -1,38 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraScreenSize : MonoBehaviour
-{
-    public SpriteRenderer area;
-    void Start()
-    {
+public class CameraScreenSize : MonoBehaviour {
 
+    [Header("Initializations")]
+    [SerializeField]
+    private SpriteRenderer _area = null;
 
-    }
-
-
-    private void Update()
-    {
-        SetSizeCamera();
-    }
-    public void SetSizeCamera()
-    {
-
+    private void Start() {
         float screenRatio = (float)Screen.width / (float)Screen.height;
-        float targetRatio = area.bounds.size.x / area.bounds.size.y;
-        if (screenRatio >= targetRatio)
-        {
-            Camera.main.orthographicSize = area.bounds.size.y / 2;
+        float targetRatio = _area.bounds.size.x / _area.bounds.size.y;
 
+        if (screenRatio >= targetRatio) {
+            Camera.main.orthographicSize = _area.bounds.size.y / 2;
+        } else {
+            float differenceInSize = targetRatio / screenRatio;
+            Camera.main.orthographicSize = _area.bounds.size.y / 2 * differenceInSize;
         }
-        else
-        {
-            float difference = targetRatio / screenRatio;
-            Camera.main.orthographicSize = area.bounds.size.y / 2 * difference;
-        }
-
-
     }
 }
 
