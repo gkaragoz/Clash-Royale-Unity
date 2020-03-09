fa1e689f7eccf8830c7837e5fa4a4ae8377046a5
gkaragoz <annotto3@gmail.com>
2020-03-09 17:06:41 +0300

    Delete UIGridSnap.cs and create UICardSnap script to reimplement all algorithm about snapping of card grids.

---
+++ Clash-Royale/Assets/Scripts/UI/UICardSnap.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class UICardSnap : MonoBehaviour {
+
+    public new Collider2D collider;
+    public Vector2 closestPoint;
+
+    private void Update() {
+        closestPoint = Physics2D.ClosestPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition), collider);
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(closestPoint, 0.25f);
+    }
+
+}
