2344669a2e852dbed5e97e5ad5bafbe4ccbcef9c
gkaragoz <annotto3@gmail.com>
2020-03-09 22:50:17 +0300

    Refactor UICardSnap.cs not completed yet. Need offset.

--- Clash-Royale/Assets/Scripts/UI/UICardSnap.cs
+++ Clash-Royale/Assets/Scripts/UI/UICardSnap.cs
@@ -2,16 +2,40 @@
 
 public class UICardSnap : MonoBehaviour {
 
-    public new Collider2D collider;
-    public Vector2 closestPoint;
+    [Header("Initializations")]
+    [SerializeField]
+    private Grid _grid = null;
+    [SerializeField]
+    private Collider2D _collider = null;
+    [SerializeField]
+    private Transform _cardSpawnIndicator = null;
+
+    [Header("Debug")]
+    [SerializeField]
+    [Utils.ReadOnly]
+    private Vector3 _closestPoint;
+    [SerializeField]
+    [Utils.ReadOnly]
+    private Camera _cam = null;
+
+    private void Awake() {
+        _cam = Camera.main;
+    }
 
     private void Update() {
-        closestPoint = Physics2D.ClosestPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition), collider);
+        _closestPoint = Physics2D.ClosestPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition), _collider);
+
+        SetCardSpawnIndicatorPosition();
     }
 
     private void OnDrawGizmos() {
         Gizmos.color = Color.blue;
-        Gizmos.DrawSphere(closestPoint, 0.25f);
+        Gizmos.DrawSphere(_closestPoint, 0.15f);
+    }
+
+    private void SetCardSpawnIndicatorPosition() {
+        Vector3 cardSpawnLogicPosition = new Vector3(_closestPoint.x / _grid.cellSize.x, _closestPoint.y / _grid.cellSize.y, 0f);
+        _cardSpawnIndicator.position = _grid.GetCellCenterWorld(Vector3Int.FloorToInt(cardSpawnLogicPosition));
     }
 
 }
