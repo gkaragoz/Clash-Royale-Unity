830eefbe770e99cfc91f4d3f302fd0a5b8f20689
YesilyurtGurkan <gurkan@ganover.com>
2020-03-09 11:04:40 +0300

    Add Camera script helps to fix camera orthagraphic size depends on screen ratio

---
+++ Clash-Royale/Assets/Scripts/UI/CameraScreenSize.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScreenSize : MonoBehaviour
+{
+
+    private Camera _cam;
+    void Start()
+    {
+
+        _cam = Camera.main;
+        SetSizeCamera();
+    }
+
+
+
+    public void SetSizeCamera()
+    {
+
+        if (_cam.aspect <= 0.48f)
+        {
+            _cam.orthographicSize = 14.75f;
+
+        }
+        else if (_cam.aspect <= 0.51f)
+        {
+            _cam.orthographicSize = 14f;
+
+        }
+        else if (_cam.aspect <= 0.57f)
+        {
+            _cam.orthographicSize = 12.5f;
+
+        }
+        else if (_cam.aspect <= 0.76f)
+        {
+            _cam.orthographicSize = 12f;
+
+        }
+
+
+    }
+}
+
+
+
