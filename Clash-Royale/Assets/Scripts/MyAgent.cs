15f2850a26d1a468e84aac3d7cae7eb4cd2ad289
gkaragoz <gkaragoz96@gmail.com>
2020-03-25 10:51:01 +0300

    Save scene and remove inheritance from MyAgent.cs

--- Clash-Royale/Assets/Scripts/MyAgent.cs
+++ Clash-Royale/Assets/Scripts/MyAgent.cs
@@ -3,36 +3,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyAgent : AIPath {
+public class MyAgent : VersionedMonoBehaviour {
 
-	public float searchTime = 0.1f;
-	public bool shouldSearch = true;
-	public Transform[] goals;
+    [Header("Initializations")]
+    [SerializeField]
+    public float searchTime = 0.1f;
+    [SerializeField]
+    public bool shouldSearch = true;
+    [SerializeField]
+    public Transform[] goals;
 
-	private List<Vector3> _goalVectors;
-	private MultiTargetPath MTP; 
+    [Header("Debug")]
+    [SerializeField]
+    [Utils.ReadOnly]
+    private List<Vector3> _goalVectors;
+    [SerializeField]
+    [Utils.ReadOnly]
+    private Vector3 _currentDestination;
+    [SerializeField]
+    [Utils.ReadOnly]
+    private Seeker _seeker;
+    [SerializeField]
+    [Utils.ReadOnly]
+    private AIPath _aiPath;
 
-	protected override void Start() {
-		base.Start();
+    private void Start() {
+        _seeker = GetComponent<Seeker>();
+        _aiPath = GetComponent<AIPath>();
 
-		_goalVectors = new List<Vector3>();
-		foreach (Transform goal in goals) {
-			_goalVectors.Add(goal.position);
-		}
+        _goalVectors = new List<Vector3>();
+        foreach (Transform goal in goals) {
+            _goalVectors.Add(goal.position);
+        }
 
-		StartCoroutine(ISearch());
+        StartCoroutine(ISearch());
     }
 
     private IEnumerator ISearch() {
-		while (shouldSearch) {
-			 MTP = seeker.StartMultiTargetPath(transform.position, _goalVectors.ToArray(), false);
-			
-			yield return new WaitForSeconds(searchTime);
-		}
-	}
+        while (shouldSearch) {
+            if (HasTargetReached()) {
+                OnTargetReached();
+            }
 
-	public override void OnTargetReached() {
-		_goalVectors.Remove(MTP.originalEndPoint);
-	}
+            MultiTargetPath MTP = _seeker.StartMultiTargetPath(transform.position, _goalVectors.ToArray(), false);
+            _currentDestination = MTP.endPoint;
+
+            Debug.Log("endPoint: " + MTP.endPoint);
+            Debug.Log("originalEndPoint: " + MTP.originalEndPoint);
+
+            yield return new WaitForSeconds(searchTime);
+        }
+    }
+
+    private bool HasTargetReached() {
+        float distance = Vector3.Distance(transform.position, _currentDestination);
+        //Debug.Log(distance + " me: " + transform.position + "  currentDestination: " + _currentDestination);
+
+        return distance <= _aiPath.endReachedDistance ? true : false;
+    }
+
+    public void OnTargetReached() {
+        _goalVectors.Remove(_currentDestination);
+    }
 
 }
